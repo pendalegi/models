457929aab3f926efa7b477c95b28164d33970feb
Саленек Александр Андреевич <AASalenek.SBT@sberbank.ru>
2018-08-14 12:56:41 +0300

    batch_and_drop_remainder (from tensorflow.contrib.data.python.ops.batching) is deprecated and will be removed in a future version. Use `tf.data.Dataset.batch(..., drop_remainder=True)`.

--- research/object_detection/builders/dataset_builder.py
+++ research/object_detection/builders/dataset_builder.py
@@ -28,7 +28,6 @@ from object_detection.data_decoders import tf_example_decoder
 from object_detection.protos import input_reader_pb2
 
 
-
 def make_initializable_iterator(dataset):
   """Creates an iterator, and initializes tables.
 
@@ -143,7 +142,8 @@ def build(input_reader_config, batch_size=None, transform_input_data_fn=None):
         process_fn,
         num_parallel_calls=num_parallel_calls)
     if batch_size:
-      dataset = dataset.apply(tf.data.Dataset.batch(batch_size, drop_remainder=True))
+      dataset = dataset.apply(
+          tf.contrib.data.batch_and_drop_remainder(batch_size))
     dataset = dataset.prefetch(input_reader_config.num_prefetch_batches)
     return dataset
 
